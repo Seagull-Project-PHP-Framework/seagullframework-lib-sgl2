985d6f5b0774265fd2c653f625594df42eaaa357
Demian Turner <demian@phpkitchen.com>
2014-01-23 18:34:33 +0000

    no message

--- src/SGL2/Bootstrap.php
+++ src/SGL2/Bootstrap.php
@@ -9,31 +9,45 @@
 namespace SGL2;
 
 
-class Bootstrap extends SGL_FrontController
+class Bootstrap extends \SGL_FrontController
 {
     public static function init()
     {
         if (!defined('SGL_INITIALISED')) {
-            SGL_FrontController::init();
+            \SGL_FrontController::init();
         }
         //  assign request to registry
-        $input = SGL_Registry::singleton();
-        $req = SGL_Request::singleton();
+        $input = \SGL_Registry::singleton();
+        $req = \SGL_Request::singleton();
 
-        if (PEAR::isError($req)) {
+        if (\PEAR::isError($req)) {
             //  stop with error page
-            SGL::displayStaticPage($req->getMessage());
+            \SGL::displayStaticPage($req->getMessage());
         }
         $input->setRequest($req);
-        $output = new SGL_Output();
+        $output = new \SGL_Output();
 
-        $process =  new SGL_Task_Init(
-            new SGL_Task_DiscoverClientOs(
-                new SGL_Task_MinimalSession(
-                    new SGL_Task_SetupLangSupport(
-                        new SGL_Void()
+        $process =  new \SGL_Task_Init(
+            new \SGL_Task_DiscoverClientOs(
+                new \SGL_Task_MinimalSession(
+                    new \SGL_Task_SetupLangSupport(
+                        new \SGL_Void()
                     ))));
 
         $process->process($input, $output);
     }
-} 
+}
+
+
+class SGL_Task_MinimalSession extends SGL_DecorateProcess
+{
+    function process($input, $output)
+    {
+        session_start();
+        $_SESSION['uid'] = 1;
+        $_SESSION['rid'] = 1;
+        $_SESSION['aPrefs'] = array();
+
+        $this->processRequest->process($input, $output);
+    }
+}
